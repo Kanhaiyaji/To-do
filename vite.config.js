8981200de8e90a059d396c073ef1c0bbe28c35c8
Kanhaiya <Kanhaiyaji@users.noreply.github.com>
2026-03-15 14:15:54 +0530

    Disable source maps to eliminate 404 errors

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/To-do/',
+  build: {
+    sourcemap: false,
+  },
   plugins: [react()],
 })
